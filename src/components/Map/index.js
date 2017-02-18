30fb3cba9a9cb38ee374a9f47812bbac0e1e3c7c
Shai Barash <shaipdx@gmail.com>
2017-02-18 09:40:07 -0800

    working-ish map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
-// import { Map, TileLayer, LayersControl } from 'react-leaflet';
-import { Map, LayersControl } from 'react-leaflet';
+import { Map, TileLayer, LayersControl } from 'react-leaflet';
 import { GoogleLayer } from 'react-leaflet-google';
 
 const { BaseLayer } = LayersControl;
 const key = 'AIzaSyCjfK-LuqlohGpxKCYZVXwg43GMQwZ9KFU'; // temp demo key
 const terrain = 'TERRAIN';
 const road = 'ROADMAP';
+const position = [51.505, -0.09];
+const zoom = 12;
 
 function TransportationMap() {
   return (
     <div>
-        <Map>
-            <BaseLayer checked name="Google Maps Roads">
-                <GoogleLayer googlekey={key}  maptype={road} />
-            </BaseLayer>
-            <BaseLayer  name="Google Maps Terrain">
-                <GoogleLayer googlekey={key}  maptype={terrain} />
-            </BaseLayer>
-        </Map>
+      <Map center={position} zoom={zoom}>
+        <LayersControl position="topright">
+          <BaseLayer name="OpenStreetMap.Mapnik">
+            <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          </BaseLayer>
+          <BaseLayer checked name="Google Maps Roads">
+            <GoogleLayer googlekey={key}  maptype={road} />
+          </BaseLayer>
+          <BaseLayer name="Google Maps Terrain">
+            <GoogleLayer googlekey={key}  maptype={terrain} />
+          </BaseLayer>
+        </LayersControl>
+      </Map>
     </div>
   );
 }
