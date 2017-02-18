f8d414bc8b0e30ed22b4c4f1a3b1970360e4764b
Shai Barash <shaipdx@gmail.com>
2017-02-18 12:17:17 -0800

    temp leaflet working with some layers

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -7,25 +7,23 @@ const key = 'AIzaSyCjfK-LuqlohGpxKCYZVXwg43GMQwZ9KFU'; // temp demo key
 const terrain = 'TERRAIN';
 const road = 'ROADMAP';
 const position = [51.505, -0.09];
-const zoom = 12;
+const zoom = 18;
 
 function TransportationMap() {
   return (
-    <div>
-      <Map center={position} zoom={zoom}>
-        <LayersControl position="topright">
-          <BaseLayer name="OpenStreetMap.Mapnik">
-            <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-          </BaseLayer>
-          <BaseLayer checked name="Google Maps Roads">
-            <GoogleLayer googlekey={key}  maptype={road} />
-          </BaseLayer>
-          <BaseLayer name="Google Maps Terrain">
-            <GoogleLayer googlekey={key}  maptype={terrain} />
-          </BaseLayer>
-        </LayersControl>
-      </Map>
-    </div>
+    <Map center={position} zoom={zoom} style={{ height: "500px" }}>
+      <LayersControl position="topright">
+        <BaseLayer name="OpenStreetMap.Mapnik">
+          <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        </BaseLayer>
+        <BaseLayer checked name="Google Maps Roads">
+          <GoogleLayer googlekey={key}  maptype={road} />
+        </BaseLayer>
+        <BaseLayer name="Google Maps Terrain">
+          <GoogleLayer googlekey={key}  maptype={terrain} />
+        </BaseLayer>
+      </LayersControl>
+    </Map>
   );
 }
 
