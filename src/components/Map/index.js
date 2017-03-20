cca39354c84875ce10f6ab9318c71bf12a85023b
aprilb <abarrett@treetopllc.com>
2017-03-19 17:06:16 -0700

    wip

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Map, TileLayer, LayersControl, GeoJSON } from 'react-leaflet';
+import { Map, TileLayer, LayersControl, GeoJSON, Popup } from 'react-leaflet';
 import { GoogleLayer } from 'react-leaflet-google';
+import request from '../../utils/request';
 
 const { BaseLayer } = LayersControl;
 const key = 'AIzaSyCjfK-LuqlohGpxKCYZVXwg43GMQwZ9KFU'; // temp demo key
@@ -12,61 +13,19 @@ const zoom = 11;
 // static test data from lines in leaflet friendly format
 // @todo: formatter
 const lineFeatures = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        dateRange: { lower: '2016-08-01', upper: '2017-10-01' },
-        coordinates: [
-          [-122.5747233253109, 45.55404608862442],
-          [-122.5712370220837, 45.55546814153171],
-          [-122.5682305216262, 45.55669265053297],
-        ],
-      },
-    },
-    {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        dateRange: { lower: '2016-09-01', upper: '2017-01-01' },
-        coordinates: [
-          [-122.6450679284365, 45.46432339252824],
-          [-122.6450629805159, 45.46768870933636],
-          [-122.6455406686514, 45.47210324880245],
-          [-122.6461913489544, 45.47751271044741],
-          [-122.6463766094138, 45.47905268909113],
-          [-122.6464042415919, 45.48003084806272],
-          [-122.6464037412303, 45.4816733032515],
-          [-122.6464082094505, 45.483920958148],
-          [-122.6463944616334, 45.48526001251474],
-        ],
-      },
-    },
-    {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        dateRange: {},
-        coordinates: [
-          [-122.6203761410378, 45.5483063111835],
-          [-122.6197873627427, 45.54830549021177],
-        ],
-      },
-    },
+  type: 'FeatureCollection'
+}
 
-  ],
-};
+request("http://localhost:8000/api/lines/", {})
+.then(function(parsedData) {
+  lineFeatures.features = parsedData.features;
+});
 
 const style = {
-  color: '#999',
+  color: 'red',
   weight: 5,
-  opacity: 0.85,
-};
+  opacity: 0
+}
 
 function TransportationMap() {
   return (
@@ -76,15 +35,22 @@ function TransportationMap() {
           <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
         </BaseLayer>
         <BaseLayer checked name="Google Maps Roads">
-          <GoogleLayer googlekey={key}  maptype={road} />
+          <GoogleLayer googlekey={key} maptype={road} />
         </BaseLayer>
         <BaseLayer name="Google Maps Terrain">
-          <GoogleLayer googlekey={key}  maptype={terrain} />
+          <GoogleLayer googlekey={key} maptype={terrain} />
         </BaseLayer>
       </LayersControl>
-      <GeoJSON data={lineFeatures} style={style} />
+      <GeoJSON data={lineFeatures} style={style} onEachFeature={onEachFeature}/>
     </Map>
   );
+}
+
+function onEachFeature(feature, layer) {
+    if (feature.properties) {
+      var fp = feature.properties;
+      layer.bindPopup(fp.sourceRef + " " + fp.dateRange + " " + fp.data.Contact.Name);
+    }
 }
 
 export default TransportationMap;
